ad34eaa502d2d441c1772fb09def181d838e969d
Cynthia Chang <aacindy1026@gmail.com>
2025-11-02 08:39:51 +0800

    新增交換物品詳情顯示功能，更新 SwapDTO、SwapService 及 swap-detail.html，改善使用者體驗

--- exchange-web-app/src/main/java/com/exchange/platform/dto/SwapDTO.java
+++ exchange-web-app/src/main/java/com/exchange/platform/dto/SwapDTO.java
@@ -7,6 +7,7 @@ import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.NONE, getterVisibility = JsonAutoDetect.Visibility.PUBLIC_ONLY, isGetterVisibility = JsonAutoDetect.Visibility.PUBLIC_ONLY)
@@ -23,6 +24,11 @@ public class SwapDTO {
     private LocalDateTime completedAt;
     private LocalDateTime aConfirmedAt;
     private LocalDateTime bConfirmedAt;
+    
+    // Proposal items information
+    private List<ProposalDTO.ProposalItemDTO> proposerItems;
+    private List<ProposalDTO.ProposalItemDTO> receiverItems;
+    private Long proposerId;
 
     @JsonProperty("aUserId")
     public Long getAUserId() {
@@ -42,5 +48,20 @@ public class SwapDTO {
     @JsonProperty("bConfirmedAt")
     public LocalDateTime getBConfirmedAt() {
         return bConfirmedAt;
+    }
+    
+    @JsonProperty("proposerItems")
+    public List<ProposalDTO.ProposalItemDTO> getProposerItems() {
+        return proposerItems;
+    }
+    
+    @JsonProperty("receiverItems")
+    public List<ProposalDTO.ProposalItemDTO> getReceiverItems() {
+        return receiverItems;
+    }
+    
+    @JsonProperty("proposerId")
+    public Long getProposerId() {
+        return proposerId;
     }
 }
